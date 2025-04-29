84a8975fd5db00ae8952a6196287cf50b7dad4bd
zerewsg <ger62076309kyc@gmail.com>
2025-04-29 22:25:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// let day = parseInt(prompt("Введите число от 1 до 7"));
+// switch(day) {
+//     case 1: console.log("Понедельник"); break;
+//     case 2: console.log("Вторник"); break;
+//     case 3: console.log("Среда"); break;
+//     case 4: console.log("Четверг"); break;
+//     case 5: console.log("Пятница"); break;
+//     case 6: console.log("Суббота"); break;
+//     case 7: console.log("Воскресенье"); break;
+//     default: console.log("Неверный день недели");
+// }
+
+
+
+// let month = parseInt(prompt("Введите месяц (1-12)"));
+// switch(month) {
+//     case 12: case 1: case 2:
+//         console.log("Зима"); break;
+//     case 3: case 4: case 5:
+//         console.log("Весна"); break;
+//     case 6: case 7: case 8:
+//         console.log("Лето"); break;
+//     case 9: case 10: case 11:
+//         console.log("Осень"); break;
+//     default: console.log("Неверный месяц");
+// }
+
+
+
+// let num = parseInt(prompt("Введите число от 1 до 3"));
+// switch(num) {
+//     case 1: console.log("Вы ввели 1");
+//     case 2: console.log("Вы ввели 2");
+//     case 3: console.log("Вы ввели 3");
+//     default: console.log("Вы ввели что-то другое");
+// }
+
+
+
+// let a = parseFloat(prompt("Введите первое число"));
+// let b = parseFloat(prompt("Введите второе число"));
+// let op = prompt("Введите операцию (+, -, *, /)");
+
+// switch(op) {
+//     case "+": console.log(a + b); break;
+//     case "-": console.log(a - b); break;
+//     case "*": console.log(a * b); break;
+//     case "/": console.log(a / b); break;
+//     default: console.log("Неизвестная операция");
+// }
